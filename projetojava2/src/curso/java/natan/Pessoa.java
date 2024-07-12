05c17ec7b260a4e6e4ebe414135a453fd7d9d490
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-07-11 21:15:10 -0500

    Classe Pessoa

--- projetojava2/src/curso/java/natan/Pessoa.java
+++ projetojava2/src/curso/java/natan/Pessoa.java
@@ -1,5 +1,28 @@
 package curso.java.natan;
 
 public class Pessoa {
+	
+	
+	private String name;
+	
+	private string sobrenome;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public string getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(string sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+		
+	
 
 }
